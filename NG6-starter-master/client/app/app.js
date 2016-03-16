fc082d1aea51de574e97c2399ff9265b72a50c31
Mark Mun <markdesign@gmail.com>
2016-03-15 22:55:20 -0700

    initial commit

---
+++ NG6-starter-master/client/app/app.js
@@ -0,0 +1,21 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import Common from './common/common';
+import Components from './components/components';
+import AppComponent from './app.component';
+import 'normalize.css';
+
+let app = angular.module('app', [
+    uiRouter,
+    Common.name,
+    Components.name
+  ]);
+
+app.config(($locationProvider) => {
+  "ngInject";
+  // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
+  // #how-to-configure-your-server-to-work-with-html5mode
+  $locationProvider.html5Mode(true).hashPrefix('!');
+});
+
+app.component('app', AppComponent);
---
+++ NG6-starter-master/client/app/components/components.js
@@ -0,0 +1,10 @@
+import angular from 'angular';
+import Home from './home/home';
+import About from './about/about';
+
+export default angular.module('app.components', [
+  Home.name,
+  About.name
+]);
+
+// export default componentModule;
